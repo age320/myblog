37a7c589550b9a9a22f020797f97c4db12e12d53
age <709191101@qq.com>
2016-07-30 15:07:38 +0800

    add user.sql

--- public/users.sql
+++ public/users.sql
@@ -1,2 +1,1 @@
-INSERT INTO `users` VALUES (1, 'admin', 'admin@admin.com', '123456', NULL, '2016-7-5 06:39:00', '2016-7-5 06:39:00');
-INSERT INTO `users` VALUES (2, 'zhang', 'zhang@zhang.com', '$2y$10$xC.GSXtJReEt2FL6ScvPoOSlcdOOIfLTw.tCjEgU2n7Eh3/bIqrB.', 'FDLb0GWRGYO9f7AYOqAuRzqcwWmu7oL4hsQdz255wa39aJ1poAeRwAlqtgIx', '2016-7-5 08:15:23', '2016-7-6 11:50:07');
+INSERT INTO `users` VALUES (3, 'age', 'zhang@zhang.com', '$2y$10$lMrVEX491gZt3EicIMbud.xmzgvtvW2DtUIBYfK9SRY3FKhleQKhq', NULL, '2016-7-30 06:27:08', '2016-7-30 06:27:08');
